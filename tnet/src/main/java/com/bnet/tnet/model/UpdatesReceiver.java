e13fca2077bdbf2b5fe9d235d0f0d024aedc3d8a
aviyay <aviyay@gmail.com>
2016-12-21 20:35:12 +0200

    refactored UpdatesReceiver a bit

--- tnet/src/main/java/com/bnet/tnet/model/UpdatesReceiver.java
+++ tnet/src/main/java/com/bnet/tnet/model/UpdatesReceiver.java
@@ -7,55 +7,52 @@ import android.database.Cursor;
 import android.net.Uri;
 
 import com.bnet.shared.model.CursorUtils;
+import com.bnet.shared.model.backend.Providable;
 import com.bnet.shared.model.backend.RepositoriesFactory;
 import com.bnet.shared.model.entities.Activity;
 import com.bnet.shared.model.entities.Business;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdatesReceiver extends BroadcastReceiver {
     public static final String PROVIDER_URI = "com.bnet.provider";
     public static final String ACTIVITIES_POSTFIX = "/activities";
     public static final String Businesses_POSTFIX = "/businesses";
+    private static List<Providable> providableList = null;
+
+    static {
+        providableList = new ArrayList<>();
+        providableList.add(new Activity());
+        providableList.add(new Business());
+    }
 
     public UpdatesReceiver() {
+
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Cursor cursor;
 
-        cursor = context.getContentResolver().query(Uri.parse("content://" + PROVIDER_URI+ACTIVITIES_POSTFIX), null, null, null, null);
+        for(Providable p : providableList) {
+            cursor = context.getContentResolver().query(Uri.parse("content://" + PROVIDER_URI + "/" + p.getURIPath()), null, null, null, null);
+            if (cursor != null) {
+                Providable temp = null;
 
-        if (cursor != null) {
-            Activity activity = new Activity();
-            cursor.moveToFirst();
+                cursor.moveToFirst();
 
-            while (!cursor.isAfterLast()) {
-                try {
-                    activity = CursorUtils.fromMatrixRow(activity, cursor);
-                } catch (Exception e) {}
+                while (!cursor.isAfterLast()) {
+                    try {
+                        temp = CursorUtils.fromMatrixRow(p, cursor);
+                    } catch (Exception e) {
+                    }
 
-                if (activity != null)
-                    RepositoriesFactory.getActivitiesRepository().addAndReturnAssignedId(activity);
+                    if (temp != null)
+                        p.getRepository().addAndReturnAssignedId(temp);
 
-                cursor.moveToNext();
-            }
-        }
-
-        cursor = context.getContentResolver().query(Uri.parse("content://"+PROVIDER_URI+Businesses_POSTFIX), null, null, null, null);
-
-        if (cursor != null) {
-            Business business = new Business();
-            cursor.moveToFirst();
-
-            while (!cursor.isAfterLast()) {
-                try {
-                    business = CursorUtils.fromMatrixRow(business, cursor);
-                } catch (Exception e) {}
-
-                if (business != null)
-                    RepositoriesFactory.getBusinessesRepository().addAndReturnAssignedId(business);
-
-                cursor.moveToNext();
+                    cursor.moveToNext();
+                }
             }
         }
     }
