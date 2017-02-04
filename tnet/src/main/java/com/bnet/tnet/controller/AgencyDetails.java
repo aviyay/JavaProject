c18013d0ea89559f191751d9278d8337eca071ff
Shy Tennenbaum <shytennenbaum@gmail.com>
2017-02-05 01:11:18 +0200

    Little Fix

--- tnet/src/main/java/com/bnet/tnet/controller/AgencyDetails.java
+++ tnet/src/main/java/com/bnet/tnet/controller/AgencyDetails.java
@@ -4,19 +4,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -224,7 +217,6 @@ public class AgencyDetails extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    @NonNull
     private FilterDecorator<Activity> getMyTravels() {
         Filter<Activity> myTravelsFilter = new Filter<Activity>() {
             @Override
